f9b7484c7463ad03a533da746a5df0cf5c3f7338
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-26 16:08:25 +0700

    imported components and added button for clearing list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+
+import List from './List';
+
+import data from './data'
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  console.log(data)
+  const [personData, setPersonData] = useState(data)
+
+  return (<main>
+    <h2 className='title'>Birthday Reminder - Starter</h2>
+    <section className='container'>
+      <h3>There are {personData.length} birthdays today!</h3>
+      <List people={personData}>
+      </List>
+      <button onClick={() => setPersonData([])} className='btn btn-block' type='button'>Clear List </button>
+    </section>
+  </main>
+  )
 };
 export default App;
